603218604e647f5f35674c6c4975b330ba362347
aperezlimon <apl897@gmail.com>
2024-06-11 20:56:41 +0200

    Inicio proyecto Django

---
+++ test/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+
+
+def saludo(request):
+    return HttpResponse('Hola, mundo!')
+
+def despedida(request):
+    return HttpResponse('Adios, mundo cruel!')
+
+def adulto(request, edad):
+    if edad >= 18:
+        return HttpResponse('Eres mayor de edad')
+    else:
+        return HttpResponse('Eres menor de edad')
